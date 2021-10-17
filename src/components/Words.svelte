47a4473dda29f2c87176c8eec99aabd5191327fd
Sanjoth J Rai <sanjr4279@gmail.com>
2021-10-17 13:26:17 +0530

    key check and space check

--- src/components/Words.svelte
+++ src/components/Words.svelte
@@ -6,34 +6,44 @@
 	// export let name: string;
     export let keyItems: number[] = [];
     export let totalWords: number = 10;
-    export let currentLetter: number|string = '', allLeters: string[] = [];
+    export let currentLetter: number|string = '', allLeters: string[] = [], currentIndex: number = 0;
 
     onMount(() => {
         for (let i = 0; i < totalWords; i++) {
             // keyItems.push(Math.floor(Math.random() * (200 - 0 + 1) + 0))
             keyItems = [...keyItems, Math.floor(Math.random() * (200 - 0 + 1) + 0)];
         }
-        currentLetter = wordsJson.words[keyItems[0]]?.substring(0,1)
         keyItems.forEach((item) => {
             let currentWord = wordsJson.words[item]
-            console.log(wordsJson.words[item])
             for(let i = 0; i < currentWord.length; i++) {
                 allLeters.push(currentWord.substring(i,i+1))
-                console.log(`[currentWord.substring(${i},${i+1})]`, currentWord.substring(i,i+1))
             }
             allLeters.push(' ');
             // allLeters.push(currentWord.substring(0,1))
         })
         console.log('[allLeters]', allLeters)
+        currentLetter = allLeters[currentIndex]
         // console.log('[key]', key)
 	});
 
     document.onkeypress = function (event) {
-        if(currentLetter === event.key) {
+        console.log('[event.keyCode]', event.keyCode)
+        if((currentLetter === event.key) || (event.keyCode === 32 && currentLetter === ' ')) {
             console.log('[correct]')
+            currentLetter = moveNext()
+            console.log('[currentLetter]', currentLetter)
+            // if correct, update current element
+
         } else {
             console.log('[wrong]')
         }
+    }
+
+    let moveNext = (): string => {
+        currentIndex = ++currentIndex;
+        console.log('[currentIndex]', currentIndex)
+        return allLeters[currentIndex];
+        // return ''
     }
 </script>
 
